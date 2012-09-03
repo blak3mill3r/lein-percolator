f56692145e9f1acced4ef7b7ee9bb5a624fee93d
Blake Miller <blak3mill3r@gmail.com>
2012-09-03 10:22:24 -0700

    add ns-tracker wicked cool dev-build cycle

--- project.clj
+++ project.clj
@@ -1,7 +1,13 @@
 (defproject lein-percolator "0.0.1-SNAPSHOT"
-  :description "A leiningen plugin for building .java files with percolator"
+  :description "Percolator Autobuilder Plugin"
+  :url "http://github.com/blak3mill3r/lein-percolator"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure          "1.4.0"]
                  [org.clojure/java.classpath   "0.2.0"]
                  [percolator/percolator        "1.0.0-SNAPSHOT"]
+                 [fs                           "1.1.2" ]
+                 [ns-tracker                   "0.1.2" ]
                  ]
-            :eval-in-leiningen true)
+  :eval-in-leiningen true)
--- src/leiningen/perc.clj
+++ src/leiningen/perc.clj
@@ -2,15 +2,45 @@
   ;(:require [clojure.java.classpath :as classpath])
   (:use [leiningen.compile :only [eval-in-project]])
   (:use [clojure.pprint :only [pprint]])
-  )
+  (:use [ns-tracker.core]))
 
+; build all percolator compilation units in the given namespace
 (defn- build-namespace [project cu-ns]
-  (eval-in-project project
-  `(percolator.core/write-all-cus-to-path '~cu-ns ~(:source-path project))
-  nil
-  nil
-  `(require 'percolator.core '~cu-ns)))
+  (eval-in-project
+    project
+    `(percolator.core/write-all-cus-to-path '~cu-ns ~(:source-path project))
+    nil
+    nil
+    `(require 'percolator.core '~cu-ns)))
 
-(defn perc [project & args]
+; build all units in all configured percolator namespaces
+(defn- full-build [project & args]
   (doseq
   [ perc-ns ( :percolator-namespaces project ) ] ( build-namespace project perc-ns )))
+
+(defn- dev-build-loop [project source-dirs opts args]
+  `(let [modified-namespaces# (ns-tracker [~@source-dirs])]
+     (while true
+       (do
+       (Thread/sleep 100)
+       (doseq [ns-sym# (modified-namespaces#)]
+         ; reload the modified clojure source
+         (require ns-sym# :reload)
+         ; build all the percolator compilation units in that namespace
+         ( percolator.core/write-all-cus-to-path ns-sym# ~(:source-path project)))))))
+
+; for development cycle
+; watch for changes and rebuild percolator namespaces as their source is modified
+(defn- dev-build [project & args]
+  (do
+    (eval-in-project
+      project
+      (dev-build-loop project [(:source-path project)] [] args)
+      nil
+      nil
+      `(require 'ns-tracker.core 'percolator.core))))
+
+(defn perc [project & args]
+  (if (some #{"dev"} args)
+    (dev-build project args)
+    (full-build project args)))

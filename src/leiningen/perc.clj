3d433745271ee974a960e7d56a24403f9a4a758b
Blake Miller <blak3mill3r@gmail.com>
2012-09-03 05:57:19 -0700

    get source path from project

--- src/leiningen/perc.clj
+++ src/leiningen/perc.clj
@@ -1,15 +1,16 @@
 (ns leiningen.perc
   ;(:require [clojure.java.classpath :as classpath])
-  (:use [leiningen.compile :only [eval-in-project]]))
+  (:use [leiningen.compile :only [eval-in-project]])
+  (:use [clojure.pprint :only [pprint]])
+  )
 
 (defn- build-namespace [project cu-ns]
-  (let [path "/home/blake/w/percolator/play/src"]  ; FIXME get path from lein project
-    (eval-in-project project
-    `(percolator.core/write-all-cus-to-path '~cu-ns ~path)
-    nil
-    nil
-    `(require 'percolator.core '~cu-ns)) ))
+  (eval-in-project project
+  `(percolator.core/write-all-cus-to-path '~cu-ns ~(:source-path project))
+  nil
+  nil
+  `(require 'percolator.core '~cu-ns)))
 
 (defn perc [project & args]
   (doseq
-    [ perc-ns ( :percolator-namespaces project ) ] ( build-namespace project perc-ns )))
+  [ perc-ns ( :percolator-namespaces project ) ] ( build-namespace project perc-ns )))
